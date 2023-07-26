13861de9c1c8f113ae5bcd55edfe3fde96a52a2e
Colton Almaraz <almarazcolton333@gmail.com>
2023-07-26 11:02:47 -0700

    Update packer bootstrap

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -1,7 +1,27 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- Only required if you have packer configured as `opt`
 vim.cmd([[packadd packer.nvim]])
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 return require("packer").startup(function(use)
 	-- Packer can manage itself
@@ -60,4 +80,7 @@ return require("packer").startup(function(use)
 			{ "L3MON4D3/LuaSnip" }, -- Required
 		},
 	})
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
