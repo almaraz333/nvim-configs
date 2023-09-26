52da3f53caabf3722c184ab26547aa810bafe362
Colton Almaraz <almarazcolton333@gmail.com>
2023-09-25 19:27:40 -0700

    Fix: remove un-needed config file

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,2 @@
 vim.g.mapleader = " "
-vim.api.nvim_set_keymap("n", "<leader>pv", ":Ex<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
