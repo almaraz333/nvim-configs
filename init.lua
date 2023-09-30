247c8150452589a946e4cdb4e1dc605ad53ea898
Colton Almaraz <almarazcolton333@gmail.com>
2023-09-30 14:19:15 -0700

    Feature: add lazygit command

--- init.lua
+++ init.lua
@@ -7,3 +7,11 @@ vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-s>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 
+-- Lazygit
+function Open_lazygit()
+	vim.cmd("!tmux split-window -h")
+	vim.cmd("!tmux select-pane -R")
+	vim.cmd("!tmux send-keys 'lazygit' Enter")
+end
+
+vim.api.nvim_set_keymap("n", "<leader>lg", ':lua Open_lazygit()<CR>', { noremap = true, silent = true })
