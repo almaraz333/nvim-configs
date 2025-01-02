1cc71bd9131c0c80d3d0cd191d62f87cdba375e9
Colton Almaraz <almarazcolton333@gmail.com>
2025-01-02 12:54:14 -0800

    feat: cleaned up the packages a bit

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,39 +1,26 @@
-local telescope = require("telescope")
-
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>gf",
-  '<cmd>lua require("telescope.builtin").git_files()<cr>',
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>ff",
-  '<cmd>lua require("telescope.builtin").find_files()<cr>',
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>fg",
-  '<cmd>lua require("telescope.builtin").live_grep()<cr>',
-  { noremap = true, silent = true }
-)
-
 return {
-  telescope.setup({
-    defaults = {
-      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+  },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 
-      vimgrep_arguments = {
-        "rg",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--ignore-case",
+        vimgrep_arguments = {
+          "rg",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--ignore-case",
+        },
       },
-    },
-  }),
+    })
+  end,
 }
